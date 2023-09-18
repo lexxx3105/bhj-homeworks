bdb2cd826d114ff2a1136a982259b7f893e001af
lexxx3105 <129284703+lexxx3105@users.noreply.github.com>
2023-09-18 22:57:23 +0300

    task.js

--- countdown/task.js
+++ countdown/task.js
@@ -1,17 +1,3 @@
-/*let timer = 59;
-let freeText = function () {
-    let output = document.getElementById("timer");
-    output.textContent -= 1;
-    if (output.textContent <= 0) {
-        alert("Вы победили в конкурсе");
-        clearInterval(inervalTimer);
-    }
-}
-let inervalTimer = setInterval(freeText, 1000);
-*/
-
-
-
 let timer = 59;
 
 let myTimer = setInterval(() => {
